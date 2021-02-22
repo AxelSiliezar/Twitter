2a11e17e22d45747856727e002924169326f88a1
Axel Siliezar <amartinezsiliezar@ucmerced.edu>
2021-02-22 10:33:42 -0800

    Ability to tweet

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -13,12 +13,36 @@ class HomeTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
 
+    let myRefreshControl = UIRefreshControl()
     
-    func loadTweet() {
-        
+    
+    @objc func loadTweets() {
+        numberOfTweet = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         
-        let myParams = ["count": 10]
+        let myParams = ["count": numberOfTweet]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            for tweet in tweets  {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+            
+        }, failure: { (Error) in
+            print("Could not retreive tweets!")
+        })
+    }
+    
+    
+    
+    func loadMoreTweets(){
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweet = numberOfTweet + 20
+        let myParams = ["count": numberOfTweet]
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
             
             self.tweetArray.removeAll()
@@ -35,13 +59,23 @@ class HomeTableViewController: UITableViewController {
     
     
     
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1  == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
+    
+    
+    
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
-        
+        loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
 
     @IBAction func onLogout(_ sender: Any) {
@@ -72,6 +106,11 @@ class HomeTableViewController: UITableViewController {
         
         
         return cell
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
     
     
